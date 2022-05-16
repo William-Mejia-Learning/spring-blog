e5d004295c34fe6ce0748d105ac1655ae5ab26fb
William Mejia <willx1995@gmail.com>
2022-05-16 13:45:43 -0500

    finishng exercises

--- src/main/java/com/codeup/springblog/controller/HelloController.java
+++ src/main/java/com/codeup/springblog/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
@@ -20,6 +21,12 @@ public class HelloController {
         return "Hello " + name + "!";
     }
     String[] names = {"William", "Javier" ,"Justin"};
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
 
     @GetMapping("/random")
     @ResponseBody
--- src/main/java/com/codeup/springblog/controller/PostController.java
+++ src/main/java/com/codeup/springblog/controller/PostController.java
@@ -1,21 +1,51 @@
 package com.codeup.springblog.controller;
 
+import com.codeup.springblog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@RequestMapping("/posts")
 public class PostController {
-    @GetMapping()
-    @ResponseBody
-    public String indexPage(){
-        return "posts index page";
+
+    public List<Post> generatePosts(){
+        List<Post> allPosts = new ArrayList<>();
+        Post post1 = new Post(1, "First post", "This is my first post!");
+        Post post2 = new Post(2, "Another post!", "Amazing content!");
+        Post post3 = new Post(3, "Third post", "Fascinating information!");
+        allPosts.add(post1);
+        allPosts.add(post2);
+        allPosts.add(post3);
+        return allPosts;
     }
 
-    @GetMapping("/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable long id){
-        return "view and individual post";
+
+    @GetMapping("/posts")
+    public String allPosts(Model model){
+        List<Post> allPosts = generatePosts();
+        model.addAttribute("allPosts", allPosts);
+        return "posts/index";
+    }
+
+    @GetMapping()
+    public String indexPage(){
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String individualPost(@PathVariable long id, Model model){
+        List<Post> allPosts = generatePosts();
+        Post post = null;
+        for (int i = 0; i < allPosts.size(); i++){
+            if (allPosts.get(i).getId() == id){
+                post = allPosts.get(i);
+            }
+        }
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/create")
