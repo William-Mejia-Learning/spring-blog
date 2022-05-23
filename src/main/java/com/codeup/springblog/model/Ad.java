a2011cdc9fbe113dddfb34d6fa64a8c005a5d4c2
williamxmejia <willx1995@gmail.com>
2022-05-22 20:50:24 -0500

    email service practice done

--- src/main/java/com/codeup/springblog/model/Ad.java
+++ src/main/java/com/codeup/springblog/model/Ad.java
@@ -1,63 +1,65 @@
-//package com.codeup.springblog.model;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "ads")
-//public class Ad {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//    private long userId;
-//    @Column(nullable = false)
-//    private String title;
-//    @Column(nullable = false)
-//    private String description;
-//
-//    public Ad(){}
-//
-//    public Ad(long id, long userId, String title, String description) {
-//        this.id = id;
-//        this.userId = userId;
-//        this.title = title;
-//        this.description = description;
-//    }
-//
-//    public Ad(long userId, String title, String description) {
-//        this.userId = userId;
-//        this.title = title;
-//        this.description = description;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public long getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(long userId) {
-//        this.userId = userId;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//}
+package com.codeup.springblog.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Ad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; //set the field for the id
+
+    //Blank / default constructor
+    public Ad(){};
+
+    //Create constructor
+
+    public Ad(String title, String description, User owner) {
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+    }
+
+
+    //Update constructor
+
+
+    //move on down to make more fields
+    @Column(nullable = false, length = 50) //NOT NULL length 100? varchar(100)?
+    private String title;
+
+    @Column(nullable = false) //Not null. . no other customization
+    private String description;
+
+    @OneToOne
+    private User owner;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+
+}
--- src/main/java/com/codeup/springblog/repositories/AdRepository.java
+++ src/main/java/com/codeup/springblog/repositories/AdRepository.java
@@ -1,14 +1,14 @@
-//package com.codeup.springblog.repositories;
-//
-//import com.codeup.springblog.model.Ad;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//
-//import java.util.List;
-//
-//public interface AdRepository extends JpaRepository<Ad, Long> {
-//    List<Ad> findByTitle(String title);
-//
-//    @Query("FROM Ad a WHERE a.title LIKE %?1%")
-//    List<Ad> searchByTitleLike(String term);
-//}
+package com.codeup.springblog.repositories;
+
+import com.codeup.springblog.model.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AdRepository extends JpaRepository<Ad, Long> {
+    List<Ad> findByTitle(String title);
+
+    @Query("FROM Ad a WHERE a.title LIKE %?1%")
+    List<Ad> searchByTitleLike(String term);
+}
